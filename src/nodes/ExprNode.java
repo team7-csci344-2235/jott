96c3729b8ef668779378cac731efe0ad5f9780a8
adrree <117570906+adrree@users.noreply.github.com>
2024-03-06 23:52:53 -0500

    ExprNode to check

--- src/nodes/ExprNode.java
+++ src/nodes/ExprNode.java
@@ -2,6 +2,7 @@ package src.nodes;
 
 import src.JottTree;
 import src.TokenDeque;
+import src.TokenType;
 
 /**
  * Class for Expr nodes
@@ -10,7 +11,37 @@ import src.TokenDeque;
  */
 public interface ExprNode extends JottTree {
     static ExprNode parseExprNode(TokenDeque tokens) throws NodeParseException {
-        // TODO: Implement
-        return null;
+
+        // Ensure first is of one of the following tokens.
+        tokens.validateFirst(TokenType.STRING, TokenType.NUMBER, TokenType.FC_HEADER, TokenType.ID_KEYWORD);
+        if (tokens.isFirstOf(TokenType.STRING)) {
+            if (tokens.isFirstOf("True") || tokens.isFirstOf("False")) {
+//                return BoolNode.parseBoolNode(tokens);
+                return null;
+                //TODO adapt to Lianna's code
+            } else {
+//                return StringLitteralNode.parseStringLitteralNode(tokens);
+                return null;
+                //TODO adapt to Lianna's code
+            }
+        } else {
+            OperandNode operandNode = OperandNode.parseOperandNode(tokens);
+            if (tokens.isFirstOf(TokenType.REL_OP)) {
+//                RelOpNode relOpNode = RelOpNode.parseRelOpNode(tokens);
+//                tokens.validateFirst(TokenType.NUMBER, TokenType.FC_HEADER, TokenType.ID_KEYWORD);
+//                OperandNode operandNode1 = OperandNode.parseOperandNode(tokens);
+//                return ...
+                return null;
+            } else if (tokens.isFirstOf(TokenType.MATH_OP)) {
+//                MathOpNode mathOpNode = MathOpNode.parseRelOpNode(tokens);
+//                tokens.validateFirst(TokenType.NUMBER, TokenType.FC_HEADER, TokenType.ID_KEYWORD);
+//                OperandNode operandNode1 = OperandNode.parseOperandNode(tokens);
+//                return ...
+                return null;
+            }
+
+            return OperandNode.parseOperandNode(tokens);
+        }
+
     }
 }
